3d315a47b600416475926723b1986377e6fb03f4
Geronimo Perez <geronimo.perez4@gmail.com>
2022-11-07 11:13:47 -0500

    added github and website buttons to applicatins

--- src/components/work/applications.js
+++ src/components/work/applications.js
@@ -1,37 +1,48 @@
 import img1 from './images/hidden-gems.png';
 import img2 from './images/README.png';
 import img3 from './images/scheduler.png';
+import img4 from './images/dashboard.png';
 
 const apps = [
   {
     key: 1,
     title: "Web App",
     img: `${img1}`,
-    description: "A discription of the app"
+    description: "A discription of the app",
+    repo: "#",
+    website: "#"
   },
   {
     key: 2,
     title: "README APP",
     img: `${img2}`,
-    description: "A discription of the app"
+    description: "A discription of the app",
+    repo: "#",
+    website: "#"
   },
   {
     key: 3,
     title: "Workday Scheduler",
     img: `${img3}`,
-    description: "A discription of the app"
+    description: "A discription of the app",
+    repo: "#",
+    website: "#"
   },
   {
     key: 4,
-    title: "App 4",
-    img: "https://picsum.photos/200/200",
-    description: "A discription of the app"
+    title: "WeatherForecast",
+    img: `${img4}`,
+    description: "Get weather forecast from any city",
+    repo: "https://github.com/Geerooniimoo/WeatherForecast",
+    website: "https://analyticsandwebdevelopment.com/WeatherForecast/"
   },
   {
     key: 5,
     title: "App 5",
     img: "https://picsum.photos/200/200",
-    description: "A discription of the app"
+    description: "A discription of the app",
+    repo: "#",
+    website: "#"
   }
 ]
 
